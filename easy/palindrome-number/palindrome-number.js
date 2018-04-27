05bbc8b91910db7b727966771fb82fe4d32df2bc
王行 <673438530@qq.com>
2018-04-27 16:26:07 +0800

    判断一个数是否是回文数

---
+++ easy/palindrome-number/palindrome-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+//join(separator)
+// 	可选。指定要使用的分隔符。如果省略该参数，则使用逗号作为分隔符。
+var isPalindrome = function(x) {
+  var result = false
+  var tempString = x.toString();
+  var resultString = tempString.split('').reverse().join("");
+  if (tempString == resultString) {
+    result = true;
+  }
+  return result;
+};
+isPalindrome(-1451);
