f7fd17e830eaaf267b7b09481d9112aea067c8d0
王行 <673438530@qq.com>
2020-05-22 10:34:52 +0800

    哈希存储元素，遍历哈希，为true返回

---
+++ leetcode/editor/cn/[面试题50]第一个只出现一次的字符.js
@@ -0,0 +1,41 @@
+//在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+//
+// 示例:
+//
+// s = "abaccdeff"
+//返回 "b"
+//
+//s = ""
+//返回 " "
+//
+//
+//
+//
+// 限制：
+//
+// 0 <= s 的长度 <= 50000
+// Related Topics 哈希表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var firstUniqChar = function(s) {
+  let map = Object.create(null);
+  for(let i = 0; i < s.length;  i ++) {
+    if (s[i] in map) {
+      map[s[i]] = false;
+    } else {
+      map[s[i]] = true;
+    }
+  }
+  for (let j in map) {
+    if (map[j]) {
+      return j;
+    }
+  }
+  return " ";
+};
+//leetcode submit region end(Prohibit modification and deletion)
