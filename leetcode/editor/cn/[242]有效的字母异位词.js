f364c5682a194b8db7ba7481e2957b25aaae8efd
王行 <673438530@qq.com>
2020-07-10 14:40:26 +0800

    转化为数组排序转换字符串

---
+++ leetcode/editor/cn/[242]有效的字母异位词.js
@@ -0,0 +1,36 @@
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+//
+// 示例 1:
+//
+// 输入: s = "anagram", t = "nagaram"
+//输出: true
+//
+//
+// 示例 2:
+//
+// 输入: s = "rat", t = "car"
+//输出: false
+//
+// 说明:
+//你可以假设字符串只包含小写字母。
+//
+// 进阶:
+//如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+// Related Topics 排序 哈希表
+// 👍 213 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if (s.length === t.length) {
+    return s.split('').sort().join() === t.split('').sort().join()
+  } else {
+    return false
+  }
+};
+//leetcode submit region end(Prohibit modification and deletion)
