16c1925456ad964b9edb67cde11a90473426cdb8
王行 <673438530@qq.com>
2020-11-13 22:14:01 +0800

    执行耗时:104 ms,击败了96.22% 的JavaScript用户 内存消耗:44.5 MB,击败了52.19% 的JavaScript用户 记录最小的索引，不断更新

---
+++ leetcode/editor/cn/[599]两个列表的最小索引总和.js
@@ -0,0 +1,61 @@
+//假设Andy和Doris想在晚餐时选择一家餐厅，并且他们都有一个表示最喜爱餐厅的列表，每个餐厅的名字用字符串表示。 
+//
+// 你需要帮助他们用最少的索引和找出他们共同喜爱的餐厅。 如果答案不止一个，则输出所有答案并且不考虑顺序。 你可以假设总是存在一个答案。 
+//
+// 示例 1: 
+//
+// 输入:
+//["Shogun", "Tapioca Express", "Burger King", "KFC"]
+//["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+//输出: ["Shogun"]
+//解释: 他们唯一共同喜爱的餐厅是“Shogun”。
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//["Shogun", "Tapioca Express", "Burger King", "KFC"]
+//["KFC", "Shogun", "Burger King"]
+//输出: ["Shogun"]
+//解释: 他们共同喜爱且具有最小索引和的餐厅是“Shogun”，它有最小的索引和1(0+1)。
+// 
+//
+// 提示: 
+//
+// 
+// 两个列表的长度范围都在 [1, 1000]内。 
+// 两个列表中的字符串的长度将在[1，30]的范围内。 
+// 下标从0开始，到列表的长度减1。 
+// 两个列表都没有重复的元素。 
+// 
+// Related Topics 哈希表 
+// 👍 88 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+    let map = new Map();
+    let set = new Set();
+    let min = Infinity;
+    for (let i = 0; i < list1.length; i ++) {
+        map.set(list1[i], i)
+    };
+    for (let j = 0; j < list2.length; j ++) {
+        if (map.has(list2[j])) {
+            if (j + map.get(list2[j]) < min) {
+                min = j + map.get(list2[j]);
+                set.clear();
+                set.add(list2[j]);
+            } else if (j + map.get(list2[j]) === min) {
+                set.add(list2[j]);
+            }
+        }
+    }
+    return Array.from(set);
+};
+//leetcode submit region end(Prohibit modification and deletion)
