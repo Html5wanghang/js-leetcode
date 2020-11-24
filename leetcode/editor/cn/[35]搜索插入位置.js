8146c0a9696f01b37a38d7e82eabfc1080f8ebb8
王行 <673438530@qq.com>
2020-11-24 22:09:11 +0800

    内存消耗:38.8 MB,击败了24.63% 的JavaScript用户 执行耗时:88 ms,击败了43.13% 的JavaScript用户 二分法解题

---
+++ leetcode/editor/cn/[35]搜索插入位置.js
@@ -0,0 +1,52 @@
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。 
+//
+// 你可以假设数组中无重复元素。 
+//
+// 示例 1: 
+//
+// 输入: [1,3,5,6], 5
+//输出: 2
+// 
+//
+// 示例 2: 
+//
+// 输入: [1,3,5,6], 2
+//输出: 1
+// 
+//
+// 示例 3: 
+//
+// 输入: [1,3,5,6], 7
+//输出: 4
+// 
+//
+// 示例 4: 
+//
+// 输入: [1,3,5,6], 0
+//输出: 0
+// 
+// Related Topics 数组 二分查找 
+// 👍 743 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length;
+    let middle = 0;
+    while (start < end) {
+        middle = Math.floor((start + end) / 2);
+        if (nums[middle] >= target) {
+            end = middle;
+        } else {
+            start = middle + 1;
+        }
+    }
+    return end;
+};
+//leetcode submit region end(Prohibit modification and deletion)
