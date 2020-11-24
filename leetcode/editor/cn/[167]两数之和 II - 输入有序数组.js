f6682567422b4997bfcb6e22ceb014ed462c7720
王行 <673438530@qq.com>
2020-11-24 22:22:19 +0800

    执行耗时:84 ms,击败了76.28% 的JavaScript用户 内存消耗:38.1 MB,击败了35.77% 的JavaScript用户 双指针解题

---
+++ leetcode/editor/cn/[167]两数之和 II - 输入有序数组.js
@@ -0,0 +1,40 @@
+//给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。 
+//
+// 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。 
+//
+// 说明: 
+//
+// 
+// 返回的下标值（index1 和 index2）不是从零开始的。 
+// 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。 
+// 
+//
+// 示例: 
+//
+// 输入: numbers = [2, 7, 11, 15], target = 9
+//输出: [1,2]
+//解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。 
+// Related Topics 数组 双指针 二分查找 
+// 👍 433 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let start = 0, end = numbers.length - 1;
+    while (start < end) {
+        if (numbers[start] + numbers[end] > target) {
+            end --;
+        } else if (numbers[start] + numbers[end] < target) {
+            start ++;
+        } else {
+            break;
+        }
+    }
+    return [start + 1, end + 1]
+};
+//leetcode submit region end(Prohibit modification and deletion)
