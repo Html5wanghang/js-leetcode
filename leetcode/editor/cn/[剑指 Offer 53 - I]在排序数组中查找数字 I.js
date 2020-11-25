3ad89d5c2094e6b5a39234594526f6a5b47a7dcd
王行 <673438530@qq.com>
2020-11-25 13:13:39 +0800

    执行耗时:80 ms,击败了84.74% 的JavaScript用户 内存消耗:38.9 MB,击败了33.27% 的JavaScript用户 单指针，比双指针要稍微慢一点，如果等，指针右移，计数，如果大，跳出循环，如果小，指针右移，不计数，返回计数

---
+++ leetcode/editor/cn/[剑指 Offer 53 - I]在排序数组中查找数字 I.js
@@ -0,0 +1,50 @@
+//统计一个数字在排序数组中出现的次数。
+//
+//
+//
+// 示例 1:
+//
+// 输入: nums = [5,7,7,8,8,10], target = 8
+//输出: 2
+//
+// 示例 2:
+//
+// 输入: nums = [5,7,7,8,8,10], target = 6
+//输出: 0
+//
+//
+//
+// 限制：
+//
+// 0 <= 数组长度 <= 50000
+//
+//
+//
+// 注意：本题与主站 34 题相同（仅返回值不同）：https://leetcode-cn.com/problems/find-first-and-last-
+//position-of-element-in-sorted-array/
+// Related Topics 数组 二分查找
+// 👍 80 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let start = 0;
+  let num = 0;
+  while (start < nums.length) {
+    if (nums[start] > target) {
+      return num;
+    } else if (nums[start] < target) {
+      start ++;
+    } else {
+      start++;
+      num++
+    }
+  }
+  return num;
+};
+//leetcode submit region end(Prohibit modification and deletion)
