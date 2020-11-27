ed08e393e9cc8d3e675c8acb5be9f3da209bc3a3
王行 <673438530@qq.com>
2020-11-27 15:51:11 +0800

    执行耗时:88 ms,击败了45.00% 的JavaScript用户 内存消耗:39.1 MB,击败了43.39% 的JavaScript用户 长度不等直接返回，s2加自身一定包括s1

---
+++ leetcode/editor/cn/[面试题 01.09]字符串轮转.js
@@ -0,0 +1,47 @@
+//字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。
+//
+// 示例1:
+//
+//  输入：s1 = "waterbottle", s2 = "erbottlewat"
+// 输出：True
+//
+//
+// 示例2:
+//
+//  输入：s1 = "aa", s2 = "aba"
+// 输出：False
+//
+//
+//
+//
+//
+// 提示：
+//
+//
+// 字符串长度在[0, 100000]范围内。
+//
+//
+// 说明:
+//
+//
+// 你能只调用一次检查子串的方法吗？
+//
+// Related Topics 字符串
+// 👍 47 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var isFlipedString = function(s1, s2) {
+  if (s1.length !== s2.length) {
+    return false
+  } else {
+    let s = s2 + s2;
+    return s.includes(s1);
+  }
+};
+//leetcode submit region end(Prohibit modification and deletion)
