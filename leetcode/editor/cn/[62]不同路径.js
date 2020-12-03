fa96139a81be476a72b24d6e887e53a27af5674a
王行 <673438530@qq.com>
2020-12-03 23:16:22 +0800

    执行耗时:88 ms,击败了39.70% 的JavaScript用户 内存消耗:38.3 MB,击败了18.81% 的JavaScript用户 每一个格子必为上一个格子或者左边格子的可能之和，在最左边或者最上面则为1

---
+++ leetcode/editor/cn/[62]不同路径.js
@@ -0,0 +1,63 @@
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。 
+//
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。 
+//
+// 问总共有多少条不同的路径？ 
+//
+// 
+//
+// 例如，上图是一个7 x 3 的网格。有多少可能的路径？ 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: m = 3, n = 2
+//输出: 3
+//解释:
+//从左上角开始，总共有 3 条路径可以到达右下角。
+//1. 向右 -> 向右 -> 向下
+//2. 向右 -> 向下 -> 向右
+//3. 向下 -> 向右 -> 向右
+// 
+//
+// 示例 2: 
+//
+// 输入: m = 7, n = 3
+//输出: 28 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= m, n <= 100 
+// 题目数据保证答案小于等于 2 * 10 ^ 9 
+// 
+// Related Topics 数组 动态规划 
+// 👍 767 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+    let arr = [];
+    for (let i = 0; i < m; i ++) {
+        arr.push([]);
+        for (let j = 0; j < n; j ++) {
+            if (i === 0 || j === 0) {
+                arr[i][j] = 1;
+            } else {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1]
+            }
+        }
+
+    }
+    return arr[m -1][n-1]
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
