95cffe4719eb93cdf0f6b7216c83555362f9a624
王行 <673438530@qq.com>
2020-12-24 17:53:23 +0800

    执行耗时:96 ms,击败了61.44% 的JavaScript用户 内存消耗:39.2 MB,击败了59.85% 的JavaScript用户 排序后取值

---
+++ leetcode/editor/cn/[215]数组中的第K个最大元素.js
@@ -0,0 +1,33 @@
+//在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+//
+// 示例 1:
+//
+// 输入: [3,2,1,5,6,4] 和 k = 2
+//输出: 5
+//
+//
+// 示例 2:
+//
+// 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+//输出: 4
+//
+// 说明:
+//
+// 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+// Related Topics 堆 分治算法
+// 👍 835 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+  nums.sort((a,b) => {
+    return b - a;
+  });
+  return nums[k - 1]
+};
+//leetcode submit region end(Prohibit modification and deletion)
