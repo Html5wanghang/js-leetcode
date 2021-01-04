a1e365266d1186489b8759f69290a3379364771b
王行 <673438530@qq.com>
2021-01-04 11:26:11 +0800

    执行耗时:100 ms,击败了44.44% 的JavaScript用户 内存消耗:40 MB,击败了29.16% 的JavaScript用户

---
+++ leetcode/editor/cn/[剑指 Offer 06]从尾到头打印链表.js
@@ -0,0 +1,39 @@
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+//
+//
+//
+// 示例 1：
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1]
+//
+//
+//
+// 限制：
+//
+// 0 <= 链表长度 <= 10000
+// Related Topics 链表
+// 👍 81 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var reversePrint = function(head) {
+  let arr = [];
+  while (head !== null) {
+    arr.push(head.val);
+    head = head.next;
+  }
+  return arr.reverse();
+};
+//leetcode submit region end(Prohibit modification and deletion)
