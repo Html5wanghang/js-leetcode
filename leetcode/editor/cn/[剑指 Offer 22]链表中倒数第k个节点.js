816b97eef2ce2034206bb5a21f9102c1408cf33a
王行 <673438530@qq.com>
2021-01-10 19:27:02 +0800

    执行耗时:88 ms,击败了59.03% 的JavaScript用户 内存消耗:39.2 MB,击败了31.51% 的JavaScript用户

---
+++ leetcode/editor/cn/[剑指 Offer 22]链表中倒数第k个节点.js
@@ -0,0 +1,40 @@
+//输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。例如，一个链表有6个节点，从头节点开始，
+//它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个节点是值为4的节点。 
+//
+// 
+//
+// 示例： 
+//
+// 给定一个链表: 1->2->3->4->5, 和 k = 2.
+//
+//返回链表 4->5. 
+// Related Topics 链表 双指针 
+// 👍 120 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var getKthFromEnd = function(head, k) {
+    let fast = head; let slow = head;
+    while (k > 0) {
+        fast = fast.next;
+        k --;
+    }
+    while (fast !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return slow;
+};
+//leetcode submit region end(Prohibit modification and deletion)
