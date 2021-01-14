482f0187f982a17b2c97fdfeb439b47685e2d459
王行 <673438530@qq.com>
2021-01-14 23:56:07 +0800

    执行耗时:88 ms,击败了84.86% 的JavaScript用户 内存消耗:39.6 MB,击败了61.90% 的JavaScript用户 删除链表的结点

---
+++ leetcode/editor/cn/[剑指 Offer 18]删除链表的节点.js
@@ -0,0 +1,60 @@
+//给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。 
+//
+// 返回删除后的链表的头节点。 
+//
+// 注意：此题对比原题有改动 
+//
+// 示例 1: 
+//
+// 输入: head = [4,5,1,9], val = 5
+//输出: [4,1,9]
+//解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+// 
+//
+// 示例 2: 
+//
+// 输入: head = [4,5,1,9], val = 1
+//输出: [4,5,9]
+//解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+// 
+//
+// 
+//
+// 说明： 
+//
+// 
+// 题目保证链表中节点的值互不相同 
+// 若使用 C 或 C++ 语言，你不需要 free 或 delete 被删除的节点 
+// 
+// Related Topics 链表 
+// 👍 83 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var deleteNode = function(head, val) {
+    if (head.val === val) {
+        return head.next
+    }
+    let temp = head;
+    while (temp.next !== null) {
+        if (temp.next.val === val) {
+            temp.next = temp.next.next;
+            return head;
+        } else {
+            temp = temp.next;
+        }
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
