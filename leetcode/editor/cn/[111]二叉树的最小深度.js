2cc61883707e87b0d588f798f47539d278db8e84
王行 <673438530@qq.com>
2021-01-17 09:58:26 +0800

    执行耗时:284 ms,击败了49.45% 的JavaScript用户 内存消耗:72.3 MB,击败了35.33% 的JavaScript用户 深度优先遍历

---
+++ leetcode/editor/cn/[111]二叉树的最小深度.js
@@ -0,0 +1,68 @@
+//给定一个二叉树，找出其最小深度。 
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。 
+//
+// 说明：叶子节点是指没有子节点的节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [2,null,3,null,4,null,5,null,6]
+//输出：5
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数的范围在 [0, 105] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 
+// 👍 429 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    let min = Infinity;
+    if (root === null) {
+        return 0
+    }
+    let dfs = (node, num) => {
+        let deep = num + 1;
+        if (node.left === null && node.right === null) {
+            min = Math.min(min, deep)
+        }
+        if (node.left !== null) {
+            dfs(node.left, deep);
+        }
+        if (node.right !== null) {
+            dfs(node.right, deep);
+        }
+    }
+    dfs(root, 0);
+    return min
+};
+//leetcode submit region end(Prohibit modification and deletion)
