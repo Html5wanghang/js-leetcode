33f3ed727903ac45c1a9220fcc3143f0064db25b
王行 <673438530@qq.com>
2021-01-21 22:57:47 +0800

    执行耗时:100 ms,击败了50.36% 的JavaScript用户 内存消耗:39.6 MB,击败了94.83% 的JavaScript用户

---
+++ leetcode/editor/cn/[328]奇偶链表.js
@@ -0,0 +1,59 @@
+//给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。 
+//
+// 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。 
+//
+// 示例 1: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 1->3->5->2->4->NULL
+// 
+//
+// 示例 2: 
+//
+// 输入: 2->1->3->5->6->4->7->NULL 
+//输出: 2->3->6->7->1->5->4->NULL 
+//
+// 说明: 
+//
+// 
+// 应当保持奇数节点和偶数节点的相对顺序。 
+// 链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。 
+// 
+// Related Topics 链表 
+// 👍 367 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    let odd = new ListNode(0); //奇
+    let even = new ListNode(0); // 偶
+    let tempOdd = odd; let tempEven = even;
+    let num = 0;
+    while (head !== null) {
+        if (num % 2 === 0) {
+            tempEven.next = head;
+            tempEven = tempEven.next;
+        } else {
+            tempOdd.next = head;
+            tempOdd = tempOdd.next;
+        }
+        head = head.next;
+        num ++;
+
+    }
+    tempOdd.next = null;
+    tempEven.next = odd.next;
+    return even.next
+};
+//leetcode submit region end(Prohibit modification and deletion)
