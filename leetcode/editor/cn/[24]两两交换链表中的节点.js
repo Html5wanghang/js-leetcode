55093296cff2f73faca1a2844cabe6ef6398c4d2
王行 <673438530@qq.com>
2021-01-22 13:37:02 +0800

    执行耗时:80 ms,击败了74.94% 的JavaScript用户 内存消耗:39 MB,击败了5.35% 的JavaScript用户

---
+++ leetcode/editor/cn/[24]两两交换链表中的节点.js
@@ -0,0 +1,79 @@
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点的数目在范围 [0, 100] 内
+// 0 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶：你能在不修改链表节点值的情况下解决这个问题吗?（也就是说，仅修改节点本身。）
+// Related Topics 递归 链表
+// 👍 788 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+  let result = new ListNode(0);
+  let temp = result;
+  let start, end;
+  while (head !== null) {
+    if (head.next === null) {
+      start = new ListNode(head.val);
+      temp.next = start;
+      head = head.next;
+    } else {
+      start = new ListNode(head.val);
+      head = head.next;
+      end = new ListNode(head.val);
+      temp.next = end;
+      temp = temp.next;
+      temp.next = start;
+      temp = temp.next;
+      head = head.next;
+    }
+  }
+  return result.next
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
