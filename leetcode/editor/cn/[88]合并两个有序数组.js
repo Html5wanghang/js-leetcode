1e7ef719592ada8f00a4c2b0f47a9ae29ec441db
王行 <673438530@qq.com>
2021-02-08 20:23:34 +0800

    执行耗时:96 ms,击败了18.64% 的JavaScript用户 内存消耗:37.8 MB,击败了84.38% 的JavaScript用户 归并排序，从数组末尾到数组头

---
+++ leetcode/editor/cn/[88]合并两个有序数组.js
@@ -0,0 +1,66 @@
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。 
+//
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nu
+//ms2 的元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//输出：[1,2,2,3,5,6]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums1 = [1], m = 1, nums2 = [], n = 0
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// nums1.length == m + n 
+// nums2.length == n 
+// 0 <= m, n <= 200 
+// 1 <= m + n <= 200 
+// -109 <= nums1[i], nums2[i] <= 109 
+// 
+// Related Topics 数组 双指针 
+// 👍 756 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let max = m + n - 1;
+    let i = m - 1;
+    let j = n - 1;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[max] = nums1[i];
+            i --
+        } else {
+            nums1[max] = nums2[j];
+            j --
+        }
+        max --;
+    }
+    while (j >= 0) {
+      nums1[j] = nums2[j];
+      j --
+    }
+
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
