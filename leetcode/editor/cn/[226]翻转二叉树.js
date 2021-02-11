f2d1a6cc0e5144ea66a2032111303e9f9f4a3a17
王行 <673438530@qq.com>
2021-02-11 09:34:14 +0800

    执行耗时:84 ms,击败了64.51% 的JavaScript用户 内存消耗:38.9 MB,击败了65.58% 的JavaScript用户 递归反转二叉树

---
+++ leetcode/editor/cn/[226]翻转二叉树.js
@@ -0,0 +1,52 @@
+//翻转一棵二叉树。 
+//
+// 示例： 
+//
+// 输入： 
+//
+//      4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9 
+//
+// 输出： 
+//
+//      4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1 
+//
+// 备注: 
+//这个问题是受到 Max Howell 的 原问题 启发的 ： 
+//
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。 
+// Related Topics 树 
+// 👍 751 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root !== null) {
+        let temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root
+    } else {
+        return null
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
