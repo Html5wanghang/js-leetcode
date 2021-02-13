81cd361c04788084b86af97a75caec9febfcce6a
王行 <673438530@qq.com>
2021-02-13 23:53:16 +0800

    执行耗时:124 ms,击败了73.97% 的JavaScript用户 内存消耗:45.1 MB,击败了78.90% 的JavaScript用户

---
+++ leetcode/editor/cn/[617]合并二叉树.js
@@ -0,0 +1,59 @@
+//给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。 
+//
+// 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点
+//。 
+//
+// 示例 1: 
+//
+// 
+//输入: 
+//	Tree 1                     Tree 2                  
+//          1                         2                             
+//         / \                       / \                            
+//        3   2                     1   3                        
+//       /                           \   \                      
+//      5                             4   7                  
+//输出: 
+//合并后的树:
+//	     3
+//	    / \
+//	   4   5
+//	  / \   \ 
+//	 5   4   7
+// 
+//
+// 注意: 合并必须从两个树的根节点开始。 
+// Related Topics 树 
+// 👍 616 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(root1, root2) {
+    if (root1 === null && root2 === null) {
+        return null
+    } else if (root1 === null && root2 !== null) {
+        return root2
+    } else if (root1 !== null && root2 === null) {
+        return root1
+    } else {
+        return new TreeNode(
+            root1.val + root2.val,
+            mergeTrees(root1.left, root2.left),
+            mergeTrees(root1.right, root2.right)
+        )
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
