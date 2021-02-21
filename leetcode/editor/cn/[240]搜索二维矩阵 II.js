11fc079ecc058a350423029c382e019c777e969e
王行 <673438530@qq.com>
2021-02-21 22:35:37 +0800

    执行耗时:432 ms,击败了65.29% 的JavaScript用户 内存消耗:40.5 MB,击败了99.13% 的JavaScript用户

---
+++ leetcode/editor/cn/[240]搜索二维矩阵 II.js
@@ -0,0 +1,65 @@
+//编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性： 
+//
+// 
+// 每行的元素从左到右升序排列。 
+// 每列的元素从上到下升序排列。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21
+//,23,26,30]], target = 5
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21
+//,23,26,30]], target = 20
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= n, m <= 300 
+// -109 <= matix[i][j] <= 109 
+// 每行的所有元素从左到右升序排列 
+// 每列的所有元素从上到下升序排列 
+// -109 <= target <= 109 
+// 
+// Related Topics 二分查找 分治算法 
+// 👍 539 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if (matrix.length === 0) {
+        return false
+    }
+    let i = 0; let j = matrix[0].length - 1;
+    while ( i < matrix.length && j >= 0) {
+        if (matrix[i][j] === target) {
+            return true
+        } else if (matrix[i][j] > target) {
+            j --
+        } else {
+            i ++
+        }
+    }
+    return false
+};
+//leetcode submit region end(Prohibit modification and deletion)
