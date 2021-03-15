b73588bf546862d1accb7a167b356390912e801d
王行 <673438530@qq.com>
2021-03-15 22:20:45 +0800

    执行耗时:148 ms,击败了20.86% 的JavaScript用户 内存消耗:44 MB,击败了32.46% 的JavaScript用户 模拟法

---
+++ leetcode/editor/cn/[6]Z 字形变换.js
@@ -0,0 +1,87 @@
+//将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。 
+//
+// 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下： 
+//
+// 
+//P   A   H   N
+//A P L S I I G
+//Y   I   R 
+//
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。 
+//
+// 请你实现这个将字符串进行指定行数变换的函数： 
+//
+// 
+//string convert(string s, int numRows); 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 3
+//输出："PAHNAPLSIIGYIR"
+// 
+//示例 2：
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 4
+//输出："PINALSIGYAHRPI"
+//解释：
+//P     I    N
+//A   L S  I G
+//Y A   H R
+//P     I
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "A", numRows = 1
+//输出："A"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 由英文字母（小写和大写）、',' 和 '.' 组成 
+// 1 <= numRows <= 1000 
+// 
+// Related Topics 字符串 
+// 👍 1053 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1) {
+        return s
+    }
+    let result = []; let row = 1; let idx = 0;
+    for (let i = 0; i < numRows; i ++) {
+        result.push([])
+    }
+    for (let j = 0; j < s.length; j ++ ) {
+        if (idx === 0) {
+            row = 1;
+        }
+        if (idx === numRows - 1) {
+            row = -1;
+        }
+        result[idx].push(s[j]);
+        idx += row;
+    }
+    let str = '';
+    for (let i = 0; i < result.length; i ++) {
+        str += result[i].join('')
+    }
+    return str
+};
+//leetcode submit region end(Prohibit modification and deletion)
