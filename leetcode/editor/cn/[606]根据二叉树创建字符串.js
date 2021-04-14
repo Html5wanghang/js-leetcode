2fa01f12448692a786842a891d5cdc20d34c8894
王行 <673438530@qq.com>
2021-04-14 11:26:42 +0800

    执行耗时:92 ms,击败了86.29% 的JavaScript用户 内存消耗:43 MB,击败了60.57% 的JavaScript用户 中序遍历创建字符串

---
+++ leetcode/editor/cn/[606]根据二叉树创建字符串.js
@@ -0,0 +1,78 @@
+//你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+//
+// 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+//
+// 示例 1:
+//
+//
+//输入: 二叉树: [1,2,3,4]
+//       1
+//     /   \
+//    2     3
+//   /
+//  4
+//
+//输出: "1(2(4))(3)"
+//
+//解释: 原本将是“1(2(4)())(3())”，
+//在你省略所有不必要的空括号对之后，
+//它将是“1(2(4))(3)”。
+//
+//
+// 示例 2:
+//
+//
+//输入: 二叉树: [1,2,3,null,4]
+//       1
+//     /   \
+//    2     3
+//     \
+//      4
+//
+//输出: "1(2()(4))(3)"
+//
+//解释: 和第一个示例相似，
+//除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
+//
+// Related Topics 树 字符串
+// 👍 187 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t
+ * @return {string}
+ */
+var tree2str = function(t) {
+    if (t === null) {
+      return ''
+    }
+    let result = '';
+    let dfs = (node) => {
+      result += node.val;
+      if (node.right !== null && node.left === null) {
+        result += '()';
+      }
+      if (node.left) {
+        result += '('
+        dfs(node.left)
+        result += ')';
+      }
+      if (node.right) {
+        result += '(';
+
+        dfs(node.right)
+        result += ')';
+      }
+    };
+    dfs(t);
+    return result
+};
+//leetcode submit region end(Prohibit modification and deletion)
